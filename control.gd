12f3bfe7cd966b7bce63fa32138d4fd703ef0bc1
DH-DoveG <3490484430@qq.com>
2023-05-09 12:29:46 +0800

    Can complete toml to dictionary parsing 3

--- control.gd
+++ control.gd
@@ -11,15 +11,15 @@ func _ready() -> void:
 	
 	time = Time.get_datetime_string_from_datetime_dict( Time.get_datetime_dict_from_system(), true )
 	
-	rtl.append_text( "BEGIN TIME > " + time + "\n\n" )
+	rtl.text += "BEGIN TIME > " + time + "\n\n"
 	
 	#test( "res://toml_file/test_all.toml" )
 	test( "res://toml_file/test.toml" )
 	
 	time = Time.get_datetime_string_from_datetime_dict( Time.get_datetime_dict_from_system(), true )
 	
-	rtl.append_text( "\n\nEND TIEM > " + time )
+	rtl.text += "\n\nEND TIEM > " + time
 
 func test( _load: String ) -> void:
 	
-	rtl.append_text( JSON.stringify( GTomlParseTool.parse_file( _load ), "\t" ) )
+	rtl.text += JSON.stringify( GTomlParseTool.parse_file( _load ), "\t" )
