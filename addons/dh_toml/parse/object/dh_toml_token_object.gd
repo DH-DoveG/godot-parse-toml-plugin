ef0f3bde117b0e95a8d23d622aac3d16202d4f2c
DH-DoveG <3490484430@qq.com>
2023-05-24 18:13:04 +0800

    2023-5-24 1

--- addons/dh_toml/parse/object/dh_toml_token_object.gd
+++ addons/dh_toml/parse/object/dh_toml_token_object.gd
@@ -7,40 +7,40 @@
 # 作者：DH_DoveG
 #===========================================
 
-class_name GTomlTokenObject
+class_name DHTomlTokenObject
 extends Node
 
 
 
 
-# Token 类型
+## Token 类型
 enum TOKEN_TYPE {
 	
-	# 空
+	## 空
 	NULL,
 	
-	# 列表
+	## 列表
 	LIST,
 	
-	# 没有辨明的类型
+	## 没有辨明的类型
 	OTHER,
 	
-	# 单引号字符串类型
+	## 单引号字符串类型
 	STRING_SINGLEQUOTE,
 	
-	# 双引号字符串类型
+	## 双引号字符串类型
 	STRING_DOUBLEQUOTATION,
 	
-	# 单引号的多行字符串类型
+	## 单引号的多行字符串类型
 	MULTILINE_STRING_SINGLEQUOTE,
 	
-	# 双引号的多行字符串类型
+	## 双引号的多行字符串类型
 	MULTILINE_STRING_DOUBLEQUOTATION,
 	
-	# 字典
+	## 字典
 	DICTIONARY,
 	
-	# 数组
+	## 数组
 	ARRAY
 	
 }
@@ -48,21 +48,21 @@ enum TOKEN_TYPE {
 
 
 
-# 类型
+## 类型
 var type: TOKEN_TYPE = TOKEN_TYPE.NULL
 
-# 行号
+## 行号
 var line: int = -1
 
-# 原始值 key 与 value
+## 原始值 key 与 value
 var original_key	: String = ""
 var original_value	: String = ""
 
-# 解析得到的 key 与 value
+## 解析得到的 key 与 value
 var parse_key	: PackedStringArray = []
 var parse_value	: Variant = null
 
-# LIST 用的
-# 看看是不是表数组
-# [[bin]] -> 这是表数组
+## LIST 用的
+## 看看是不是表数组
+## [[bin]] -> 这是表数组
 var is_list_array: bool = false
--- addons/dh_toml/parse/value/dh_toml_parse_number.gd
+++ addons/dh_toml/parse/value/dh_toml_parse_number.gd
@@ -7,7 +7,7 @@
 # 作者：DH-DoveG
 #===========================================
 
-class_name GTomlParseNumber
+class_name DHTomlParseNumber
 extends Node
 
 
@@ -29,32 +29,32 @@ func _init() -> void:
 
 
 # 解析值，尝试解析为数字
-func parse( _value: String ) -> Variant:
+func parse(_value: String) -> Variant:
 	
 	regex.clear()
 	
-	var inf_nan: Variant = _is_inf_or_nan( _value )
+	var inf_nan: Variant = _is_inf_or_nan(_value)
 	
 	if inf_nan != null:
 		
 		return inf_nan
 	
 	# 说起来 toml 是支持 100_000 这种东西的，先将 _ 去掉
-	_value = _value.replace( "_", "" )
+	_value = _value.replace("_", "")
 	
 	# 二进制
-	if _is_bin( _value ):
+	if _is_bin(_value):
 		
 		return _value.bin_to_int()
 	
 	# 八进制
-	elif _is_octal( _value ):
+	elif _is_octal(_value):
 		
 		var num		: int = _value.substr(2).to_int()
 		var decimal	: int = 0
 		var base	: int = 1
 		
-		while( num > 0 ):
+		while(num > 0):
 			
 			var lastDigit: int = num % 10
 			num = num / 10
@@ -64,12 +64,12 @@ func parse( _value: String ) -> Variant:
 		return decimal
 	
 	# 十六进制
-	elif _is_hexadecimal( _value ):
+	elif _is_hexadecimal(_value):
 		
 		return _value.hex_to_int()
 	
 	# 浮点
-	elif _is_float( _value ):
+	elif _is_float(_value):
 		
 		return _value.to_float()
 	
@@ -79,7 +79,7 @@ func parse( _value: String ) -> Variant:
 		return _value.to_int()
 	
 	# 科学计数法如：1.2e1
-	elif _is_scientific_counting( _value ):
+	elif _is_scientific_counting(_value):
 		
 		return _value
 	
@@ -90,13 +90,13 @@ func parse( _value: String ) -> Variant:
 
 
 # 判断字符串是否是无穷或者不是数
-func _is_inf_or_nan( _value: String ) -> Variant:
+func _is_inf_or_nan(_value: String) -> Variant:
 	
 	pattern = "^[+-]?(inf|nan)$"
 	
 	regex.compile(pattern)
 	
-	var res: RegExMatch = regex.search( _value )
+	var res: RegExMatch = regex.search(_value)
 	
 	if res:
 		
@@ -124,58 +124,58 @@ func _is_inf_or_nan( _value: String ) -> Variant:
 
 
 # 判断字符串是否是二进制
-func _is_bin( _value: String ) -> bool:
+func _is_bin(_value: String) -> bool:
 	
 	pattern = "^0b[01]+(_[01]+)*$"
 	
-	regex.compile( pattern )
+	regex.compile(pattern)
 	
-	return regex.search( _value ) != null
+	return regex.search(_value) != null
 
 
 
 
 # 判断字符串是否是八进制
-func _is_octal( _value: String ) -> bool:
+func _is_octal(_value: String) -> bool:
 	
-	pattern = "^0o[0-7]+(_[0-7]+)*$"
+	pattern = "^0o[0-7]+([0-7]+)*$"
 	
-	regex.compile( pattern )
+	regex.compile(pattern)
 	
-	return regex.search( _value ) != null
+	return regex.search(_value) != null
 
 
 
 
 # 判断是否浮点
-func _is_float( _value: String ) -> bool:
+func _is_float(_value: String) -> bool:
 	
-	pattern = "^[+-]?(0|[1-9][0-9]*(_[0-9]+)*)\\.[0-9]+(_[0-9]+)*$"
+	pattern = "^[+-]?(0|[1-9][0-9]*([0-9]+)*)\\.[0-9]+([0-9]+)*$"
 	
-	regex.compile( pattern )
+	regex.compile(pattern)
 	
-	return regex.search( _value ) != null
+	return regex.search(_value) != null
 
 
 
 
 # 判断字符串是否是十六进制
-func _is_hexadecimal( _value: String ) -> bool:
+func _is_hexadecimal(_value: String) -> bool:
 	
-	pattern = "^0x[0-9A-Fa-f]+(_[0-9A-Fa-f]+)*$"
+	pattern = "^0x[0-9A-Fa-f]+([0-9A-Fa-f]+)*$"
 	
-	regex.compile( pattern )
+	regex.compile(pattern)
 	
-	return regex.search( _value ) != null
+	return regex.search(_value) != null
 
 
 
 
 # 判断字符串是不是科学计数法表示的
-func _is_scientific_counting( _value: String ) -> bool:
+func _is_scientific_counting(_value: String) -> bool:
 	
-	pattern = "^[+-]?(0|[1-9][0-9]*(_[0-9]+)*)(\\.[0-9]+(_[0-9]+)*)?[eE][+-]?[0-9]+(_[0-9]+)*$"
+	pattern = "^[+-]?(0|[1-9][0-9]*([0-9]+)*)(\\.[0-9]+([0-9]+)*)?[eE][+-]?[0-9]+([0-9]+)*$"
 	
-	regex.compile( pattern )
+	regex.compile(pattern)
 	
-	return regex.search( _value ) != null
+	return regex.search(_value) != null
